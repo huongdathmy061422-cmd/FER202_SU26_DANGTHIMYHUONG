486b2eb9f5ccb656007a3bb6979d07760b3a95ff
huongdathmy061422-cmd <huongdathmy061422@gmail.com>
2026-05-13 16:43:17 +0700

    update

---
+++ code/slot2/ex1/src/demo.js
@@ -0,0 +1,15 @@
+chao();
+
+let chao = () => console.log("Xinchao");
+
+chao1("Alice");
+chao1();
+
+let person ={
+    id: 1,
+    name: "Bob",
+    age: 30,
+    address: "123 MAin St"
+};
+
+let chao2 = (person) => console.log('Xin chao, ${person}');
